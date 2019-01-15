fafb4252e2ee630661fca4dab81bbc9b49e17360
leehywo <leehywo@gmail.com>
2019-01-15 22:11:42 +0900

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>WEB</title>
+    </head>
+    <body>
+        <h1>WEB</h1>
+        <ol>
+            <li>HTML</li>
+        </ol>
+        <h2>Welcome</h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur cum sit reiciendis! Nostrum natus a labore, provident quod ducimus blanditiis excepturi perferendis autem quam, consequatur facere accusamus unde ab voluptates.
+    </body>
+</html>
